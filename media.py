3c0d19a06a9c4519bee50b366e89740b2dc6e9ff
Carlo Rossy <carlorossy@carlos-mbp.attlocal.net>
2018-05-08 10:30:57 -0400

    Add untracked files, delete video argmument in Movie class

--- media.py
+++ media.py
@@ -1,6 +1,6 @@
 import webbrowser
 
-class Movie(Video):
+class Movie():
     """This class provides a way to store movie related information"""
     VALID_RATINGS = ["G", "PG", "PG-13", "R"]# THIS IS A CONSTANT, AND STYLE GUIDE STATES ITS BEST IF IT IS IN CAP.
     def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
